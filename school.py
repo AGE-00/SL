3297b11cd2a03bacf4acff8409fe929431d45a15
students <riku.rtx3090.jp@gmail.com>
2024-07-28 10:10:00 +0900

    試しに

---
+++ school.py
@@ -0,0 +1,13 @@
+import torch
+import cv2
+# Model loading
+model = torch.hub.load("ultralytics/yolov5", "yolov5s")  # Can be 'yolov5n' - 'yolov5x6', or 'custom'
+
+# Inference on images
+img = "https://ultralytics.com/images/zidane.jpg"  # Can be a file, Path, PIL, OpenCV, numpy, or list of images
+
+# Run inference
+results = model(img)
+
+# Display results
+results.print()  # Other options: .show(), .save(), .crop(), .pandas(), etc.
