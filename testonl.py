596dcf6ae09bbc726d7bc9771f8c34979d964dda
AGEAGE <115388084+AGE-00@users.noreply.github.com>
2024-10-16 01:56:25 +0000

    change:yolov11 test

--- testonl.py
+++ testonl.py
@@ -1,6 +1,10 @@
-import numpy as np
-import ultralytics
-import cv2
-import math
-import torch
-import tensorflow as tf
+from ultralytics import YOLO
+
+# Load a COCO-pretrained YOLO11n model
+model = YOLO("yolo11n.pt")
+
+# Train the model on the COCO8 example dataset for 100 epochs
+results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
+
+# Run inference with the YOLO11n model on the 'bus.jpg' image
+results = model("mysearch-1/train/images/-2024-08-19-105836_png.rf.27f2393f0eda22d996b15d05d7e2a945.jpg")
