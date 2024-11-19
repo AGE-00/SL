a0f9e0e583ec91bb34a633c52d26a009a5660b4e
age <riku.rtx3090.jp@gmail.com>
2024-11-20 02:02:20 +0900

    succes

--- mysearch-1/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# mysearch > 2024-08-19 4:34am
-https://universe.roboflow.com/age-unvlj/mysearch
-
-Provided by a Roboflow user
-License: MIT
-
--- testonl.py
+++ testonl.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("C:/myprograms/SL/yolo11n.pt")
 
 # Train the model on the COCO8 example dataset for 100 epochs
-results = model.train(data="C:/Myprograms/SL/mysearch-1/data.yaml", epochs=100, imgsz=640)
+results = model.train(data="C:/Users/nagas/Downloads/mysearch.v1i.yolov11/data.yaml", epochs=40, imgsz=640 ,batch=0.5)   
