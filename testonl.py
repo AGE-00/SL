73e72cc8d83542865d7ce1efe24f27fa31366b1d
age <riku.rtx3090.jp@gmail.com>
2024-11-20 00:57:08 +0900

    ignore:add:some

--- testonl.py
+++ testonl.py
@@ -1,9 +1,7 @@
 from ultralytics import YOLO
 
-# Load a model
-model = YOLO("yolo11n.yaml")  # build a new model from YAML
-model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
-model = YOLO("yolo11n.yaml").load("yolo11n.pt")  # build from YAML and transfer weights
+# Load a COCO-pretrained YOLO11n model
+model = YOLO("C:/myprograms/SL/yolo11n.pt")
 
-# Train the model
-results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
+# Train the model on the COCO8 example dataset for 100 epochs
+results = model.train(data="C:/Myprograms/SL/mysearch-1/data.yaml", epochs=100, imgsz=640)
