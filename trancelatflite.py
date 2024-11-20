2c5bdb7ed4be4a0bf0ba971c18f61ede3c4d46cf
students <riku.rtx3090.jp@gmail.com>
2024-11-20 13:54:12 +0900

    conf

--- trancelatflite.py
+++ trancelatflite.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 # Load the YOLO11 model
 model = YOLO("C:/Users/students/Documents/GitHub/SL/train15/weights/best.pt")
 
-results = model(0,show=True,conf=0.5) 
+results = model(0,show=True,conf=0.6) 
