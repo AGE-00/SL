9cc7420b15a6eb92342b7a130d65db046fbb2442
students <riku.rtx3090.jp@gmail.com>
2024-11-20 12:11:00 +0900

    update:olo11

--- trancelatflite.py
+++ trancelatflite.py
@@ -1,13 +1,6 @@
 from ultralytics import YOLO
 
 # Load the YOLO11 model
-model = YOLO("yolo11n.pt")
+model = YOLO("C:/Users/students/Documents/GitHub/SL/train15/weights/best.pt")
 
-# Export the model to TFLite format
-model.export(format="tflite")  # creates 'yolo11n_float32.tflite'
-
-# Load the exported TFLite model
-tflite_model = YOLO("yolo11n_float32.tflite")
-
-# Run inference
-results = tflite_model("https://ultralytics.com/images/bus.jpg")
+results = model(0,show=True,conf=0.5) 
