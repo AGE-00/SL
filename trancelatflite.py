4cc02fecfbd92979242965af2cc512fac53c4652
students <riku.rtx3090.jp@gmail.com>
2024-12-04 11:48:45 +0900

    update:yolo change sorce code

--- trancelatflite.py
+++ trancelatflite.py
@@ -1,6 +1,4 @@
 from ultralytics import YOLO
 
-# Load the YOLO11 model
 model = YOLO("C:/Users/students/Documents/GitHub/SL/train15/weights/best.pt")
-
-results = model(0,show=True,conf=0.6) 
+model.export(format="tflite")
